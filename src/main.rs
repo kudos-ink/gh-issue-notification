627bebfa2ce04a618e5684ff344bc4429b06df9d
Connor Campbell <ccampbell.james@gmail.com>
2024-05-01 10:50:29 +0100

    feat: add logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use aws_lambda_events::event::ses::SimpleEmailEvent;
-use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
+use lambda_runtime::{
+    run, service_fn,
+    tracing::{self, error, info},
+    Error, LambdaEvent,
+};
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -10,27 +14,40 @@ struct Response {
 }
 
 async fn function_handler(event: LambdaEvent<SimpleEmailEvent>) -> Result<Response, Error> {
+    info!("Event payload: {:?}", event.payload);
     let records = event.payload.records;
 
     let message_id = records
         .first()
-        .ok_or_else(|| Error::from("No email record found"))?
+        .ok_or_else(|| {
+            error!("No email record found");
+            Error::from("No email record found")
+        })?
         .ses
         .mail
         .message_id
         .as_ref()
-        .ok_or_else(|| Error::from("No message id found"))?;
+        .ok_or_else(|| {
+            error!("No message id found");
+            Error::from("No message id found")
+        })?;
 
     let trimmed = message_id.trim_matches(&['<', '>']);
     let parts: Vec<&str> = trimmed.split('/').collect();
     if parts.len() < 4 {
+        error!("Malformed messaged id: {message_id}");
         return Err(Error::from(format!("Malformed messaged id: {message_id}")));
     }
 
     let owner = parts[0].to_string();
     let repo = parts[1].to_string();
-    let issue_number = parts[3].parse()?;
 
+    let issue_number = parts[3].parse::<u64>().map_err(|e| {
+        error!("Failed to parse issue number: {}", e);
+        Error::from(format!("Error parsing issue number: {}", e))
+    })?;
+
+    info!("Output - owner: {owner}, repo: {repo}, issue_number: {issue_number}");
     Ok(Response {
         owner,
         repo,
